b50984171d2d935744810597948f6aa503895e41
Vikas Jha <jhavikas2004@gmail.com>
2026-05-05 00:19:21 +0530

    refactored lspconfig

--- nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/lua/kickstart/plugins/lspconfig.lua
@@ -83,35 +83,36 @@ return {
       end
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      local servers = {
-        jdtls = {
-          cmd = { 'jdtls' },
-          root_dir = require('lspconfig.util').root_pattern('pom.xml', 'gradle.build', '.git'),
-          capabilities = require('cmp_nvim_lsp').default_capabilities(),
-        },
-        pyright = {},
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
-      }
-      local ensure_installed = vim.tbl_keys(servers or {})
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+       local servers = {
+         jdtls = {
+           cmd = { 'jdtls' },
+           root_dir = require('lspconfig.util').root_pattern('pom.xml', 'gradle.build', '.git'),
+           capabilities = require('cmp_nvim_lsp').default_capabilities(),
+         },
+         pyright = {},
+         lua_ls = {
+           settings = {
+             Lua = {
+               completion = {
+                 callSnippet = 'Replace',
+               },
+             },
+           },
+         },
+       }
 
-      require('mason-lspconfig').setup {
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
+       local ensure_installed = vim.tbl_keys(servers or {})
+       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+       require('mason-lspconfig').setup {
+         handlers = {
+           function(server_name)
+             local server = servers[server_name] or {}
+             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+             require('lspconfig')[server_name].setup(server)
+           end,
+         },
+       }
     end,
   },
 }
